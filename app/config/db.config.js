7db069e55e3f893949c44276bb5d25a7fd619aa0
Vesselin Stanev <78701093+Vasi81@users.noreply.github.com>
2022-05-20 20:54:25 +0200

    update db.config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  url: "mongodb://localhost:27017/oficinas"
-  /*url: " mongodb://oficina:A2345Edix@127.0.0.1:27017/oficinas?authSource=admin",
-  comercialDefault: "624eba235900533ba8a72fcc",
+  url: "mongodb://localhost:27017/oficinas",
+  /*url: " mongodb://oficina:A2345Edix@127.0.0.1:27017/oficinas?authSource=admin",*/
+  comercialDefault: "62700a345f995c303439ef15",
   host: "tasaciones-cbre-es.correoseguro.dinaserver.com",
   port: 465,
   user: "edix@tasaciones.cbre.es",
   pass: "muzjek-diwpyw-Kykgu3",
   from: 'edix@tasaciones.cbre.es'
-  */
+  
 };
