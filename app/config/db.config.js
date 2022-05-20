d56d3e478b4c81b847a5a55a00b8ec3b58ad618f
dueactive <37436467+dueactive@users.noreply.github.com>
2022-05-20 21:47:53 +0200

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,7 +3,9 @@ module.exports = {
   url: "mongodb://localhost:27017/oficinas",
   /*Instancia mongo en contendor docker mongodb */
   /*url: " mongodb://oficina:A2345Edix@127.0.0.1:27017/oficinas?authSource=admin",*/
+  /*Commercial default for sending generic email.*/
   comercialDefault: "62700a345f995c303439ef15",
+  /*setup email*/
   host: "tasaciones-cbre-es.correoseguro.dinaserver.com",
   port: 465,
   user: "edix@tasaciones.cbre.es",
